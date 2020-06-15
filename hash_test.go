f0718c2cde41ce30eb91e324766e5cf29e26d94b
Keoni Gandall <koeng101@gmail.com>
2020-06-15 12:28:05 -0700

    Fixed Seqhash -> seqhash in hash_test.go

--- hash_test.go
+++ hash_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	want := "4f0fc3582aeb0667d268bab48a8237036cc7334abb2269b42c6fa25fa4ce9938"
-	if got := Seqhash("Hello World", true); got != want {
+	if got := seqhash("Hello World", true); got != want {
 		t.Errorf("Seqhash(\"Hello World\", true) = %q, want %q", got, want)
 	}
 }
