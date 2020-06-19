99bc4ee081a5e28a912cba99ac333a3528d5c85a
Timothy Stiles <tim@stiles.io>
2020-06-18 17:36:53 -0700

    made variable and function names more readable.

--- transformations.go
+++ transformations.go
@@ -4,7 +4,7 @@ import (
 	"math/rand"
 	"time"
 
-	wr "github.com/mroth/weightedrand"
+	weightedRand "github.com/mroth/weightedrand"
 )
 
 // Codon holds information for a codon triplet in a struct
@@ -27,69 +27,69 @@ type CodonTable struct {
 }
 
 // Generate map of amino acid -> codon chooser
-func (codonTable CodonTable) optimizationTable() map[string]wr.Chooser {
+func (codonTable CodonTable) generateOptimizationTable() map[string]weightedRand.Chooser {
 	rand.Seed(time.Now().UTC().UnixNano())
-	var optMap = make(map[string]wr.Chooser)
-	for _, aa := range codonTable.AminoAcids {
+	var optimizationMap = make(map[string]weightedRand.Chooser)
+	for _, aminoAcid := range codonTable.AminoAcids {
 		// Get list of triplets and their weights
-		codonChoices := make([]wr.Choice, len(aa.Codons))
+		codonChoices := make([]weightedRand.Choice, len(aminoAcid.Codons))
 		totals := 0
-		for _, codon := range aa.Codons {
-			codonChoices = append(codonChoices, wr.Choice{Item: codon.Triplet, Weight: uint(codon.Occurrence)})
+		for _, codon := range aminoAcid.Codons {
+			codonChoices = append(codonChoices, weightedRand.Choice{Item: codon.Triplet, Weight: uint(codon.Occurrence)})
 			totals += codon.Occurrence
 		}
-		optMap[aa.Letter] = wr.NewChooser(codonChoices...)
+		optimizationMap[aminoAcid.Letter] = weightedRand.NewChooser(codonChoices...)
 	}
-	return optMap
+	return optimizationMap
 }
 
 // Optimize takes an amino acid sequence and CodonTable and returns an optimized codon sequence
 func Optimize(aminoAcids string, codonTable CodonTable) string {
 	var codons string
-	optTable := codonTable.optimizationTable()
-	for _, aa := range aminoAcids {
-		codons += optTable[string(aa)].Pick().(string)
+	optimizationTable := codonTable.generateOptimizationTable()
+	for _, aminoAcid := range aminoAcids {
+		codons += optimizationTable[string(aminoAcid)].Pick().(string)
 	}
 	return codons
 }
 
 // Generate map of codons -> amino acid
-func (codonTable CodonTable) translationTable() map[string]string {
-	var transMap = make(map[string]string)
-	for _, aa := range codonTable.AminoAcids {
-		for _, codon := range aa.Codons {
-			transMap[codon.Triplet] = aa.Letter
+func (codonTable CodonTable) generateTranslationTable() map[string]string {
+	var translationMap = make(map[string]string)
+	for _, aminoAcid := range codonTable.AminoAcids {
+		for _, codon := range aminoAcid.Codons {
+			translationMap[codon.Triplet] = aminoAcid.Letter
 		}
 	}
-	return transMap
+	return translationMap
 }
 
 // Translate translates a codon sequence to an amino acid sequence
 func Translate(nucleotides string, codonTable CodonTable) string {
 	var aminoAcids string
-	transTable := codonTable.translationTable()
-	l := len(nucleotides) / 3 // Assumes input sequences are divisble by 3
-	for i := 0; l > i; i++ {
-		aminoAcids += transTable[nucleotides[i*3:(i+1)*3]]
+	translationTable := codonTable.generateTranslationTable()
+	length := len(nucleotides) / 3 // Assumes input sequences are divisible by 3
+	for i := 0; length > i; i++ {
+		aminoAcids += translationTable[nucleotides[i*3:(i+1)*3]]
 	}
 	return aminoAcids
 }
 
 // Function to generate default codon tables from NCBI https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi
-func genTable(aas, starts string) CodonTable {
+func generateCodonTable(aas, starts string) CodonTable {
 	base1 := "TTTTTTTTTTTTTTTTCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGG"
 	base2 := "TTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGG"
 	base3 := "TCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAG"
 	// Add triplets to an amino acid -> triplet map, and if a possible start codon, add to start codon list
-	var aaMap = make(map[rune][]Codon)
+	var aminoAcidMap = make(map[rune][]Codon)
 	var startCodons []string
 	var stopCodons []string
-	for i, aa := range aas {
-		if _, ok := aaMap[aa]; ok == false {
-			aaMap[aa] = []Codon{}
+	for i, aminoAcid := range aas {
+		if _, ok := aminoAcidMap[aminoAcid]; ok == false {
+			aminoAcidMap[aminoAcid] = []Codon{}
 		}
 		triplet := string([]byte{base1[i], base2[i], base3[i]})
-		aaMap[aa] = append(aaMap[aa], Codon{triplet, 0})
+		aminoAcidMap[aminoAcid] = append(aminoAcidMap[aminoAcid], Codon{triplet, 0})
 		if starts[i] == 77 { // M rune
 			startCodons = append(startCodons, triplet)
 		}
@@ -98,47 +98,45 @@ func genTable(aas, starts string) CodonTable {
 		}
 	}
 	// Convert amino acid -> triplet map to an amino acid list
-	var aaList []AminoAcid
-	for k, v := range aaMap {
-		aaList = append(aaList, AminoAcid{string(k), v})
+	var aminoAcidSlice []AminoAcid
+	for k, v := range aminoAcidMap {
+		aminoAcidSlice = append(aminoAcidSlice, AminoAcid{string(k), v})
 	}
-	return CodonTable{startCodons, stopCodons, aaList}
+	return CodonTable{startCodons, stopCodons, aminoAcidSlice}
 }
 
 // DefaultCodonTables stores all codon tables published by NCBI https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi
 var DefaultCodonTables = map[int]CodonTable{
-	1:  genTable("FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "---M------**--*----M---------------M----------------------------"),
-	2:  genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSS**VVVVAAAADDEEGGGG", "----------**--------------------MMMM----------**---M------------"),
-	3:  genTable("FFLLSSSSYY**CCWWTTTTPPPPHHQQRRRRIIMMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**----------------------MM---------------M------------"),
-	4:  genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--MM------**-------M------------MMMM---------------M------------"),
-	5:  genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSSSVVVVAAAADDEEGGGG", "---M------**--------------------MMMM---------------M------------"),
-	6:  genTable("FFLLSSSSYYQQCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
-	9:  genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG", "----------**-----------------------M---------------M------------"),
-	10: genTable("FFLLSSSSYY**CCCWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**-----------------------M----------------------------"),
-	11: genTable("FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "---M------**--*----M------------MMMM---------------M------------"),
-	12: genTable("FFLLSSSSYY**CC*WLLLSPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**--*----M---------------M----------------------------"),
-	13: genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSGGVVVVAAAADDEEGGGG", "---M------**----------------------MM---------------M------------"),
-	14: genTable("FFLLSSSSYYY*CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG", "-----------*-----------------------M----------------------------"),
-	16: genTable("FFLLSSSSYY*LCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------*---*--------------------M----------------------------"),
-	21: genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNNKSSSSVVVVAAAADDEEGGGG", "----------**-----------------------M---------------M------------"),
-	22: genTable("FFLLSS*SYY*LCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "------*---*---*--------------------M----------------------------"),
-	23: genTable("FF*LSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--*-------**--*-----------------M--M---------------M------------"),
-	24: genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDEEGGGG", "---M------**-------M---------------M---------------M------------"),
-	25: genTable("FFLLSSSSYY**CCGWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "---M------**-----------------------M---------------M------------"),
-	26: genTable("FFLLSSSSYY**CC*WLLLAPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**--*----M---------------M----------------------------"),
-	27: genTable("FFLLSSSSYYQQCCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
-	28: genTable("FFLLSSSSYYQQCCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**--*--------------------M----------------------------"),
-	29: genTable("FFLLSSSSYYYYCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
-	30: genTable("FFLLSSSSYYEECC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
-	31: genTable("FFLLSSSSYYEECCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**-----------------------M----------------------------"),
-	33: genTable("FFLLSSSSYYY*CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDEEGGGG", "---M-------*-------M---------------M---------------M------------")}
+	1:  generateCodonTable("FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "---M------**--*----M---------------M----------------------------"),
+	2:  generateCodonTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSS**VVVVAAAADDEEGGGG", "----------**--------------------MMMM----------**---M------------"),
+	3:  generateCodonTable("FFLLSSSSYY**CCWWTTTTPPPPHHQQRRRRIIMMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**----------------------MM---------------M------------"),
+	4:  generateCodonTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--MM------**-------M------------MMMM---------------M------------"),
+	5:  generateCodonTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSSSVVVVAAAADDEEGGGG", "---M------**--------------------MMMM---------------M------------"),
+	6:  generateCodonTable("FFLLSSSSYYQQCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
+	9:  generateCodonTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG", "----------**-----------------------M---------------M------------"),
+	10: generateCodonTable("FFLLSSSSYY**CCCWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**-----------------------M----------------------------"),
+	11: generateCodonTable("FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "---M------**--*----M------------MMMM---------------M------------"),
+	12: generateCodonTable("FFLLSSSSYY**CC*WLLLSPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**--*----M---------------M----------------------------"),
+	13: generateCodonTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSSGGVVVVAAAADDEEGGGG", "---M------**----------------------MM---------------M------------"),
+	14: generateCodonTable("FFLLSSSSYYY*CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNNKSSSSVVVVAAAADDEEGGGG", "-----------*-----------------------M----------------------------"),
+	16: generateCodonTable("FFLLSSSSYY*LCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------*---*--------------------M----------------------------"),
+	21: generateCodonTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNNKSSSSVVVVAAAADDEEGGGG", "----------**-----------------------M---------------M------------"),
+	22: generateCodonTable("FFLLSS*SYY*LCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "------*---*---*--------------------M----------------------------"),
+	23: generateCodonTable("FF*LSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--*-------**--*-----------------M--M---------------M------------"),
+	24: generateCodonTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDEEGGGG", "---M------**-------M---------------M---------------M------------"),
+	25: generateCodonTable("FFLLSSSSYY**CCGWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "---M------**-----------------------M---------------M------------"),
+	26: generateCodonTable("FFLLSSSSYY**CC*WLLLAPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**--*----M---------------M----------------------------"),
+	27: generateCodonTable("FFLLSSSSYYQQCCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
+	28: generateCodonTable("FFLLSSSSYYQQCCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**--*--------------------M----------------------------"),
+	29: generateCodonTable("FFLLSSSSYYYYCC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
+	30: generateCodonTable("FFLLSSSSYYEECC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "--------------*--------------------M----------------------------"),
+	31: generateCodonTable("FFLLSSSSYYEECCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**-----------------------M----------------------------"),
+	33: generateCodonTable("FFLLSSSSYYY*CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDEEGGGG", "---M-------*-------M---------------M---------------M------------")}
 
 //func main() {
 //	rand.Seed(time.Now().UTC().UnixNano())
 //	codonTable := CodonTable{[]string{},[]string{},[]aminoAcid{{"M",[]Codon{{"ATG", 1}}},{"G",[]Codon{{"GGA", 1}}},{"*",[]Codon{{"TAA",1},{"TGA",1}}}}}
-//	trans := translate("ATGGGCTGA", defaultCodonTables[1])
-//	fmt.Println(optimize(trans,codonTable))
-//	fmt.Println(optimize(trans,codonTable))
-//	fmt.Println(optimize(trans,codonTable))
-//	fmt.Println(optimize(trans,codonTable))
+//	translation := Translate("ATGGGCTGA", DefaultCodonTables[1])
+
+//	fmt.Println(optimize(translation,codonTable))
 //}
