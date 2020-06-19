161210759258f7791cf38ea15049d7333ee09496
Timothy Stiles <tim@stiles.io>
2020-06-18 17:09:34 -0700

    renamed struct names to be more readable.

--- transformations.go
+++ transformations.go
@@ -7,43 +7,46 @@ import (
 	wr "github.com/mroth/weightedrand"
 )
 
-type codon struct {
-	triplet    string
-	occurrence int
+// Codon holds information for a codon triplet in a struct
+type Codon struct {
+	Triplet    string
+	Occurrence int
 }
 
-type aminoAcid struct {
-	letter string
-	codons []codon
+// AminoAcid holds information for an amino acid and related codons in a struct in a struct
+type AminoAcid struct {
+	Letter string
+	Codons []Codon
 }
 
-type codonTable struct {
-	startCodons []string
-	stopCodons  []string
-	aminoAcid   []aminoAcid
+// CodonTable holds information for a codon table.
+type CodonTable struct {
+	StartCodons []string
+	StopCodons  []string
+	AminoAcids  []AminoAcid
 }
 
 // Generate map of amino acid -> codon chooser
-func (ct codonTable) optimizationTable() map[string]wr.Chooser {
+func (codonTable CodonTable) optimizationTable() map[string]wr.Chooser {
 	rand.Seed(time.Now().UTC().UnixNano())
 	var optMap = make(map[string]wr.Chooser)
-	for _, aa := range ct.aminoAcid {
+	for _, aa := range codonTable.AminoAcids {
 		// Get list of triplets and their weights
-		codonChoices := make([]wr.Choice, len(aa.codons))
+		codonChoices := make([]wr.Choice, len(aa.Codons))
 		totals := 0
-		for _, codon := range aa.codons {
-			codonChoices = append(codonChoices, wr.Choice{Item: codon.triplet, Weight: uint(codon.occurrence)})
-			totals += codon.occurrence
+		for _, codon := range aa.Codons {
+			codonChoices = append(codonChoices, wr.Choice{Item: codon.Triplet, Weight: uint(codon.Occurrence)})
+			totals += codon.Occurrence
 		}
-		optMap[aa.letter] = wr.NewChooser(codonChoices...)
+		optMap[aa.Letter] = wr.NewChooser(codonChoices...)
 	}
 	return optMap
 }
 
-//Optimize takes an amino acid sequence and CodonTable and returns an optimized codon sequence
-func Optimize(aminoAcids string, ct codonTable) string {
+// Optimize takes an amino acid sequence and CodonTable and returns an optimized codon sequence
+func Optimize(aminoAcids string, codonTable CodonTable) string {
 	var codons string
-	optTable := ct.optimizationTable()
+	optTable := codonTable.optimizationTable()
 	for _, aa := range aminoAcids {
 		codons += optTable[string(aa)].Pick().(string)
 	}
@@ -51,20 +54,20 @@ func Optimize(aminoAcids string, ct codonTable) string {
 }
 
 // Generate map of codons -> amino acid
-func (ct codonTable) translationTable() map[string]string {
+func (codonTable CodonTable) translationTable() map[string]string {
 	var transMap = make(map[string]string)
-	for _, aa := range ct.aminoAcid {
-		for _, codon := range aa.codons {
-			transMap[codon.triplet] = aa.letter
+	for _, aa := range codonTable.AminoAcids {
+		for _, codon := range aa.Codons {
+			transMap[codon.Triplet] = aa.Letter
 		}
 	}
 	return transMap
 }
 
 // Translate translates a codon sequence to an amino acid sequence
-func Translate(nucleotides string, ct codonTable) string {
+func Translate(nucleotides string, codonTable CodonTable) string {
 	var aminoAcids string
-	transTable := ct.translationTable()
+	transTable := codonTable.translationTable()
 	l := len(nucleotides) / 3 // Assumes input sequences are divisble by 3
 	for i := 0; l > i; i++ {
 		aminoAcids += transTable[nucleotides[i*3:(i+1)*3]]
@@ -73,20 +76,20 @@ func Translate(nucleotides string, ct codonTable) string {
 }
 
 // Function to generate default codon tables from NCBI https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi
-func genTable(aas, starts string) codonTable {
+func genTable(aas, starts string) CodonTable {
 	base1 := "TTTTTTTTTTTTTTTTCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGG"
 	base2 := "TTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGGTTTTCCCCAAAAGGGG"
 	base3 := "TCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAGTCAG"
 	// Add triplets to an amino acid -> triplet map, and if a possible start codon, add to start codon list
-	var aaMap = make(map[rune][]codon)
+	var aaMap = make(map[rune][]Codon)
 	var startCodons []string
 	var stopCodons []string
 	for i, aa := range aas {
 		if _, ok := aaMap[aa]; ok == false {
-			aaMap[aa] = []codon{}
+			aaMap[aa] = []Codon{}
 		}
 		triplet := string([]byte{base1[i], base2[i], base3[i]})
-		aaMap[aa] = append(aaMap[aa], codon{triplet, 0})
+		aaMap[aa] = append(aaMap[aa], Codon{triplet, 0})
 		if starts[i] == 77 { // M rune
 			startCodons = append(startCodons, triplet)
 		}
@@ -95,15 +98,15 @@ func genTable(aas, starts string) codonTable {
 		}
 	}
 	// Convert amino acid -> triplet map to an amino acid list
-	var aaList []aminoAcid
+	var aaList []AminoAcid
 	for k, v := range aaMap {
-		aaList = append(aaList, aminoAcid{string(k), v})
+		aaList = append(aaList, AminoAcid{string(k), v})
 	}
-	return codonTable{startCodons, stopCodons, aaList}
+	return CodonTable{startCodons, stopCodons, aaList}
 }
 
 // DefaultCodonTables stores all codon tables published by NCBI https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi
-var DefaultCodonTables = map[int]codonTable{
+var DefaultCodonTables = map[int]CodonTable{
 	1:  genTable("FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "---M------**--*----M---------------M----------------------------"),
 	2:  genTable("FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIMMTTTTNNKKSS**VVVVAAAADDEEGGGG", "----------**--------------------MMMM----------**---M------------"),
 	3:  genTable("FFLLSSSSYY**CCWWTTTTPPPPHHQQRRRRIIMMTTTTNNKKSSRRVVVVAAAADDEEGGGG", "----------**----------------------MM---------------M------------"),
@@ -132,10 +135,10 @@ var DefaultCodonTables = map[int]codonTable{
 
 //func main() {
 //	rand.Seed(time.Now().UTC().UnixNano())
-//	ct := codonTable{[]string{},[]string{},[]aminoAcid{{"M",[]codon{{"ATG", 1}}},{"G",[]codon{{"GGA", 1}}},{"*",[]codon{{"TAA",1},{"TGA",1}}}}}
+//	codonTable := CodonTable{[]string{},[]string{},[]aminoAcid{{"M",[]Codon{{"ATG", 1}}},{"G",[]Codon{{"GGA", 1}}},{"*",[]Codon{{"TAA",1},{"TGA",1}}}}}
 //	trans := translate("ATGGGCTGA", defaultCodonTables[1])
-//	fmt.Println(optimize(trans,ct))
-//	fmt.Println(optimize(trans,ct))
-//	fmt.Println(optimize(trans,ct))
-//	fmt.Println(optimize(trans,ct))
+//	fmt.Println(optimize(trans,codonTable))
+//	fmt.Println(optimize(trans,codonTable))
+//	fmt.Println(optimize(trans,codonTable))
+//	fmt.Println(optimize(trans,codonTable))
 //}
