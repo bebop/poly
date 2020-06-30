c9d05059ab569a71f343cfac44168f12e504503e
Timothy Stiles <tim@stiles.io>
2020-06-30 16:29:00 -0700

    fixed docs/ url path bug.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ module.exports = {
       {
         docs: {
           // It is recommended to set document id as docs home page (`docs/` path).
-          homePageId: 'doc1',
+          homePageId: 'installation',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
