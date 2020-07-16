0022e282873ed22048ec5f63c6de2169ae066192
Timothy Stiles <tim@stiles.io>
2020-07-16 13:24:24 -0700

    getFeatureSequence now works with nested Location struct.

--- sequence.go
+++ sequence.go
@@ -64,13 +64,13 @@ func complementBase(basePair rune) rune {
 	return ComplementBaseRuneMap[basePair]
 }
 
-func parseGbkLocation(locationString string, complement bool) []Location {
-	var locations []Location
+func parseGbkLocation(locationString string) Location {
+	var location Location
 	if !(strings.ContainsAny(locationString, "(")) { // Case checks for simple expression of x..x
 		startEndSplit := strings.Split(locationString, "..")
 		start, _ := strconv.Atoi(startEndSplit[0])
 		end, _ := strconv.Atoi(startEndSplit[1])
-		locations = append(locations, Location{Start: start - 1, End: end, Complement: complement})
+		location = Location{Start: start - 1, End: end}
 	} else {
 		firstOuterParentheses := strings.Index(locationString, "(")
 		expression := locationString[firstOuterParentheses+1 : strings.LastIndex(locationString, ")")]
@@ -90,72 +90,45 @@ func parseGbkLocation(locationString string, complement bool) []Location {
 						ParenthesesCount--
 					}
 				}
-				locations = append(parseGbkLocation(expression[:firstInnerParentheses+comma+1], complement), parseGbkLocation(expression[2+firstInnerParentheses+comma:], complement)...)
+				location.SubLocations = append(location.SubLocations, parseGbkLocation(expression[:firstInnerParentheses+comma+1]), parseGbkLocation(expression[2+firstInnerParentheses+comma:]))
 			} else { // This is the default join(x..x,x..x)
 				for _, numberRange := range strings.Split(expression, ",") {
-					locations = append(locations, parseGbkLocation(numberRange, complement)...)
+					location.SubLocations = append(location.SubLocations, parseGbkLocation(numberRange))
 				}
 			}
 
 		case "complement":
-			locations = append(locations, parseGbkLocation(expression, true)...)
+			subLocation := parseGbkLocation(expression)
+			subLocation.Complement = true
+			location.SubLocations = append(location.SubLocations, subLocation)
 		}
 	}
-	return locations
+	return location
+}
+
+func getFeatureSequenceHelper(feature Feature, location Location) string {
+	var sequenceBuffer bytes.Buffer
+	var sequenceString string
+	parentSequence := feature.ParentAnnotatedSequence.Sequence.Sequence
+	if len(location.SubLocations) == 0 {
+		sequenceBuffer.WriteString(parentSequence[location.Start:location.End])
+	} else {
+		for _, subLocation := range location.SubLocations {
+			sequenceBuffer.WriteString(getFeatureSequenceHelper(feature, subLocation))
+		}
+	}
+
+	if location.Complement {
+		sequenceString = ReverseComplement(sequenceBuffer.String())
+	} else {
+		sequenceString = sequenceBuffer.String()
+	}
+
+	return sequenceString
 }
 
 func getFeatureSequence(feature Feature) string {
-	var sequenceBuffer bytes.Buffer
-	locations := parseGbkLocation(feature.GbkLocationString, false)
-	for _, location := range locations {
-		sequenceString := feature.ParentAnnotatedSequence.Sequence.Sequence[location.Start:location.End]
-		if location.Complement {
-			sequenceBuffer.WriteString(ReverseComplement(sequenceString))
-		} else {
-			sequenceBuffer.WriteString(sequenceString)
-		}
-	}
-	return sequenceBuffer.String()
+	location := parseGbkLocation(feature.GbkLocationString)
+
+	return getFeatureSequenceHelper(feature, location)
 }
-
-// getFeatureSequence is a recursive function to get the sequence of a feature, given that feature's Location and full parent sequence
-// func getFeatureSequence(locationString string, fullSeq string) string {
-// 	if !(strings.ContainsAny(locationString, "(")) { // Case checks for simple expression of x..x
-// 		startEndSplit := strings.Split(locationString, "..")
-// 		start, _ := strconv.Atoi(startEndSplit[0])
-// 		end, _ := strconv.Atoi(startEndSplit[1])
-// 		return fullSeq[start-1 : end] // Sequences are indexed at 1, but inclusive of last element
-// 	} else {
-// 		firstOuterParentheses := strings.Index(locationString, "(")
-// 		expression := locationString[firstOuterParentheses+1 : strings.LastIndex(locationString, ")")]
-// 		switch command := locationString[0:firstOuterParentheses]; command {
-// 		case "join":
-// 			// This case checks for join(complement(x..x),complement(x..x)), or any more complicated derivatives
-// 			if strings.ContainsAny(expression, "(") {
-// 				firstInnerParentheses := strings.Index(expression, "(")
-// 				ParenthesesCount := 1
-// 				comma := 0
-// 				for i := 1; ParenthesesCount > 0; i++ { // "(" is at 0, so we start at 1
-// 					comma = i
-// 					switch expression[firstInnerParentheses+i] {
-// 					case []byte("(")[0]:
-// 						ParenthesesCount++
-// 					case []byte(")")[0]:
-// 						ParenthesesCount--
-// 					}
-// 				}
-// 				return getFeatureSequence(expression[:firstInnerParentheses+comma+1], fullSeq) + getFeatureSequence(expression[2+firstInnerParentheses+comma:], fullSeq)
-// 			} else { // This is the default join(x..x,x..x)
-// 				joinedSequence := ""
-// 				for _, numberRange := range strings.Split(expression, ",") {
-// 					joinedSequence += getFeatureSequence(numberRange, fullSeq)
-// 				}
-// 				return joinedSequence
-// 			}
-
-// 		case "complement":
-// 			return ReverseComplement(getFeatureSequence(expression, fullSeq))
-// 		}
-// 	}
-// 	return "failed"
-// }
