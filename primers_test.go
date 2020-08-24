37b2d282e99b62f7c99b10f850c10d5963653703
jecalles <callesjonathan@gmail.com>
2020-08-24 15:20:31 -0700

    implements CalcTM, which calculates primer melt temp (no mismatches) (#34)
    * implements CalcTM, which calculates primer melt temp (no mismatches)
    
    * tidies up go.mod and go.sum
    
    * renamed variables in primer.go
    
    * changed function names for primers.go.
    
    * updated go sum and fixed variable definition.
    
    Co-authored-by: jecalles <jecalles@users.noreply.github.com>
    Co-authored-by: Tim <TimothyStiles@users.noreply.github.com>

---
+++ primers_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"math"
+	"testing"
+)
+
+func TestMarmurDoty(t *testing.T) {
+	testSeq := "ACGTCCGGACTT"
+	expectedTM := 31.0
+	if calcTM := MarmurDoty(testSeq); expectedTM != calcTM {
+		t.Errorf("MarmurDoty has changed on test. Got %f instead of %f", calcTM, expectedTM)
+	}
+}
+
+func TestSantaLucia(t *testing.T) {
+	testSeq := "ACGATGGCAGTAGCATGC" //"GTAAAACGACGGCCAGT" // M13 fwd
+	testCPrimer := 0.1e-6
+	testCNa := 350e-3
+	testCMg := 0.0
+	expectedTM := 62.7
+	if calcTM, _, _ := SantaLucia(testSeq, testCPrimer, testCNa, testCMg); math.Abs(expectedTM-calcTM)/expectedTM >= 0.02 {
+		t.Errorf("SantaLucia has changed on test. Got %f instead of %f", calcTM, expectedTM)
+	}
+}
+
+func TestCalcTM(t *testing.T) {
+	testSeq := "GTAAAACGACGGCCAGT" // M13 fwd
+	expectedTM := 52.8
+	if calcTM := MeltingTemp(testSeq); math.Abs(expectedTM-calcTM)/expectedTM >= 0.02 {
+		t.Errorf("MeltingTemp has changed on test. Got %f instead of %f", calcTM, expectedTM)
+	}
+}
