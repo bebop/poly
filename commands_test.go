9e55a717d9fa48bf3b4f70dfacf6f554d399e447
Timothy Stiles <tim@stiles.io>
2020-10-21 15:32:21 -0700

    backing up progress before yet another refactor.

--- commands_test.go
+++ commands_test.go
@@ -1,15 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"os"
-	"os/exec"
-	"runtime"
-	"strings"
+	"sync"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 /******************************************************************************
@@ -34,116 +32,199 @@ TODO:
 write subtest to check for empty output before merge
 ******************************************************************************/
 
-func TestConvert(t *testing.T) {
+func TestPipe(t *testing.T) {
 
-	if runtime.GOOS == "windows" {
-		fmt.Println("TestConvert was not run and autopassed. Currently Poly command line support is not available for windows. See https://github.com/TimothyStiles/poly/issues/16.")
-	} else {
-
-		// testing redirected pipe output
-		command := "cat data/puc19.gbk | poly c -i gbk -o json > data/converttest.json"
-		exec.Command("bash", "-c", command).Output()
-
-		// getting test sequence from non-pipe io to compare against redirected io
-		baseTestSequence := ReadGbk("data/puc19.gbk")
-		outputTestSequence := ReadJSON("data/converttest.json")
-
-		// cleaning up test data
-		os.Remove("data/converttest.json")
-
-		// diff original sequence and converted sequence reread back into
-		// AnnotatedSequence format. If there's an error fail test and print diff.
-		if diff := cmp.Diff(baseTestSequence, outputTestSequence, cmpopts.IgnoreFields(Feature{}, "ParentAnnotatedSequence")); diff != "" {
-			t.Errorf(" mismatch from convert pipe input test (-want +got):\n%s", diff)
-		}
-
-		//clearing possibly still existent prior test data.
-		os.Remove("data/ecoli-mg1655.json")
-		os.Remove("data/puc19.json")
-
-		// testing multithreaded non piped output
-		command = "poly c -o json data/puc19.gbk data/ecoli-mg1655.gff"
-		exec.Command("bash", "-c", command).Output()
-
-		ecoliInputTestSequence := ReadGff("data/ecoli-mg1655.gff")
-		ecoliOutputTestSequence := ReadJSON("data/ecoli-mg1655.json")
-
-		//clearing test data.
-		os.Remove("data/ecoli-mg1655.json")
-
-		// compared input gff from resulting output json. Fail test and print diff if error.
-		if diff := cmp.Diff(ecoliInputTestSequence, ecoliOutputTestSequence, cmpopts.IgnoreFields(Feature{}, "ParentAnnotatedSequence")); diff != "" {
-			t.Errorf(" mismatch from concurrent gff input test (-want +got):\n%s", diff)
-		}
-
-		bsubInputTestSequence := ReadGbk("data/puc19.gbk")
-		bsubOutputTestSequence := ReadJSON("data/puc19.json")
-
-		// clearing test data.
-		os.Remove("data/puc19.json")
-
-		// compared input gbk from resulting output json. Fail test and print diff if error.
-		if diff := cmp.Diff(bsubInputTestSequence, bsubOutputTestSequence, cmpopts.IgnoreFields(Feature{}, "ParentAnnotatedSequence")); diff != "" {
-			t.Errorf(" mismatch from concurrent gbk input test (-want +got):\n%s", diff)
-		}
-
+	r, stdinWriter, err := os.Pipe()
+	if err != nil {
 	}
+	fmt.Println(isPipe())
+
+	origStdin := os.Stdin
+	_, err = stdinWriter.Write([]byte(BuildGbk(ReadGbk("data/puc19.gbk"))))
+
+	if err != nil {
+		stdinWriter.Close()
+		os.Stdin = origStdin
+	}
+
+	os.Stdin = r
+	stdinWriter.Close()
+
+	args := os.Args[0:1]                   // Name of the program.
+	args = append(args, "ha", "-i", "gbk") // Append a flag
+	results := captureOutput(func() { run(args) })
+	fmt.Print(results)
+	r.Close()
+	os.Stdin = origStdin
 }
 
-func TestHash(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		fmt.Println("TestHash was not run and autopassed. Currently Poly command line support is not available for windows. See https://github.com/TimothyStiles/poly/issues/16.")
-	} else {
+// func TestConvert(t *testing.T) {
 
-		puc19GbkBlake3Hash := "4b0616d1b3fc632e42d78521deb38b44fba95cca9fde159e01cd567fa996ceb9"
+// 	if runtime.GOOS == "windows" {
+// 		fmt.Println("TestConvert was not run and autopassed. Currently Poly command line support is not available for windows. See https://github.com/TimothyStiles/poly/issues/16.")
+// 	} else {
 
-		// testing pipe input
-		command := "cat data/puc19.gbk | poly hash -i gbk"
-		hashOutput, _ := exec.Command("bash", "-c", command).Output()
-		hashOutputString := strings.TrimSpace(string(hashOutput))
+// 		// testing redirected pipe output
+// 		command := "cat data/puc19.gbk | poly c -i gbk -o json > data/converttest.json"
+// 		exec.Command("bash", "-c", command).Output()
 
-		if hashOutputString != puc19GbkBlake3Hash {
-			t.Errorf("TestHash for piped input has failed. Returned %q, want %q", hashOutputString, puc19GbkBlake3Hash)
-		}
+// 		// getting test sequence from non-pipe io to compare against redirected io
+// 		baseTestSequence := ReadGbk("data/puc19.gbk")
+// 		outputTestSequence := ReadJSON("data/converttest.json")
 
-		// testing regular input
-		command = "poly hash data/puc19.gbk"
-		hashOutput, _ = exec.Command("bash", "-c", command).Output()
-		hashOutputString = strings.TrimSpace(string(hashOutput))
+// 		// cleaning up test data
+// 		os.Remove("data/converttest.json")
 
-		if hashOutputString != puc19GbkBlake3Hash {
-			t.Errorf("TestHash for regular input has failed. Returned %q, want %q", hashOutputString, puc19GbkBlake3Hash)
-		}
+// 		// diff original sequence and converted sequence reread back into
+// 		// AnnotatedSequence format. If there's an error fail test and print diff.
+// 		if diff := cmp.Diff(baseTestSequence, outputTestSequence, cmpopts.IgnoreFields(Feature{}, "ParentAnnotatedSequence")); diff != "" {
+// 			t.Errorf(" mismatch from convert pipe input test (-want +got):\n%s", diff)
+// 		}
 
-		// testing json write output
-		command = "poly hash -o json data/puc19.gbk"
-		exec.Command("bash", "-c", command).Output()
-		hashOutputString = ReadJSON("data/puc19.json").Sequence.Hash
-		os.Remove("data/puc19.json")
+// 		//clearing possibly still existent prior test data.
+// 		os.Remove("data/ecoli-mg1655.json")
+// 		os.Remove("data/puc19.json")
 
-		if hashOutputString != puc19GbkBlake3Hash {
-			t.Errorf("TestHash for json write output has failed. Returned %q, want %q", hashOutputString, puc19GbkBlake3Hash)
-		}
+// 		// testing multithreaded non piped output
+// 		command = "poly c -o json data/puc19.gbk data/ecoli-mg1655.gff"
+// 		exec.Command("bash", "-c", command).Output()
 
+// 		ecoliInputTestSequence := ReadGff("data/ecoli-mg1655.gff")
+// 		ecoliOutputTestSequence := ReadJSON("data/ecoli-mg1655.json")
+
+// 		//clearing test data.
+// 		os.Remove("data/ecoli-mg1655.json")
+
+// 		// compared input gff from resulting output json. Fail test and print diff if error.
+// 		if diff := cmp.Diff(ecoliInputTestSequence, ecoliOutputTestSequence, cmpopts.IgnoreFields(Feature{}, "ParentAnnotatedSequence")); diff != "" {
+// 			t.Errorf(" mismatch from concurrent gff input test (-want +got):\n%s", diff)
+// 		}
+
+// 		bsubInputTestSequence := ReadGbk("data/puc19.gbk")
+// 		bsubOutputTestSequence := ReadJSON("data/puc19.json")
+
+// 		// clearing test data.
+// 		os.Remove("data/puc19.json")
+
+// 		// compared input gbk from resulting output json. Fail test and print diff if error.
+// 		if diff := cmp.Diff(bsubInputTestSequence, bsubOutputTestSequence, cmpopts.IgnoreFields(Feature{}, "ParentAnnotatedSequence")); diff != "" {
+// 			t.Errorf(" mismatch from concurrent gbk input test (-want +got):\n%s", diff)
+// 		}
+
+// 	}
+// }
+
+// func TestHash(t *testing.T) {
+// 	if runtime.GOOS == "windows" {
+// 		fmt.Println("TestHash was not run and autopassed. Currently Poly command line support is not available for windows. See https://github.com/TimothyStiles/poly/issues/16.")
+// 	} else {
+
+// 		puc19GbkBlake3Hash := "4b0616d1b3fc632e42d78521deb38b44fba95cca9fde159e01cd567fa996ceb9"
+
+// 		// testing pipe input
+// 		command := "cat data/puc19.gbk | poly hash -i gbk"
+// 		hashOutput, _ := exec.Command("bash", "-c", command).Output()
+// 		hashOutputString := strings.TrimSpace(string(hashOutput))
+
+// 		if hashOutputString != puc19GbkBlake3Hash {
+// 			t.Errorf("TestHash for piped input has failed. Returned %q, want %q", hashOutputString, puc19GbkBlake3Hash)
+// 		}
+
+// 		// testing regular input
+// 		command = "poly hash data/puc19.gbk"
+// 		hashOutput, _ = exec.Command("bash", "-c", command).Output()
+// 		hashOutputString = strings.TrimSpace(string(hashOutput))
+
+// 		if hashOutputString != puc19GbkBlake3Hash {
+// 			t.Errorf("TestHash for regular input has failed. Returned %q, want %q", hashOutputString, puc19GbkBlake3Hash)
+// 		}
+
+// 		// testing json write output
+// 		command = "poly hash -o json data/puc19.gbk"
+// 		exec.Command("bash", "-c", command).Output()
+// 		hashOutputString = ReadJSON("data/puc19.json").Sequence.Hash
+// 		os.Remove("data/puc19.json")
+
+// 		if hashOutputString != puc19GbkBlake3Hash {
+// 			t.Errorf("TestHash for json write output has failed. Returned %q, want %q", hashOutputString, puc19GbkBlake3Hash)
+// 		}
+
+// 	}
+// }
+
+// func TestOptimizeCommand(t *testing.T) {
+// 	if runtime.GOOS == "windows" {
+// 		fmt.Println("TestOptimize was not run and autopassed. Currently Poly command line support is not available for windows. See https://github.com/TimothyStiles/poly/issues/16.")
+// 	} else {
+
+// 		gfpTranslation := "MASKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTFSYGVQCFSRYPDHMKRHDFFKSAMPEGYVQERTISFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYITADKQKNGIKANFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITHGMDELYK*"
+
+// 		command := "echo " + gfpTranslation + " |" + "poly op -aa"
+// 		optimizeOutput, _ := exec.Command("bash", "-c", command).Output()
+// 		optimizeOutputString := strings.TrimSpace(string(optimizeOutput))
+// 		translation := Translate(optimizeOutputString, DefaultCodonTablesByName["Standard"])
+
+// 		if translation != gfpTranslation {
+// 			t.Errorf("TestOptimizeCommand for json write output has failed. Returned %q, want %q", translation, gfpTranslation)
+// 		}
+
+// 	}
+
+// }
+
+// spoofPipe is a helper function to spoof stdin for testing pipe workflows without a bunch of cmd.exec calls
+func spoofPipe(bytes []byte, function func()) string {
+	r, stdinWriter, err := os.Pipe()
+	if err != nil {
 	}
+	fmt.Println(isPipe())
+
+	origStdin := os.Stdin
+	_, err = stdinWriter.Write(bytes)
+
+	if err != nil {
+		stdinWriter.Close()
+		os.Stdin = origStdin
+	}
+
+	os.Stdin = r
+	stdinWriter.Close()
+
+	args := os.Args[0:1]                   // Name of the program.
+	args = append(args, "ha", "-i", "gbk") // Append a flag
+	results := captureOutput(func() { function() })
+	fmt.Print(results)
+	r.Close()
+	os.Stdin = origStdin
+
+	return results
 }
-
-func TestOptimizeCommand(t *testing.T) {
-	if runtime.GOOS == "windows" {
-		fmt.Println("TestOptimize was not run and autopassed. Currently Poly command line support is not available for windows. See https://github.com/TimothyStiles/poly/issues/16.")
-	} else {
-
-		gfpTranslation := "MASKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTFSYGVQCFSRYPDHMKRHDFFKSAMPEGYVQERTISFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYITADKQKNGIKANFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITHGMDELYK*"
-
-		command := "echo " + gfpTranslation + " |" + "poly op -aa"
-		optimizeOutput, _ := exec.Command("bash", "-c", command).Output()
-		optimizeOutputString := strings.TrimSpace(string(optimizeOutput))
-		translation := Translate(optimizeOutputString, DefaultCodonTablesByName["Standard"])
-
-		if translation != gfpTranslation {
-			t.Errorf("TestOptimizeCommand for json write output has failed. Returned %q, want %q", translation, gfpTranslation)
-		}
-
+func captureOutput(f func()) string {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		panic(err)
 	}
-
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+		log.SetOutput(os.Stderr)
+	}()
+	os.Stdout = writer
+	os.Stderr = writer
+	log.SetOutput(writer)
+	out := make(chan string)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		var buf bytes.Buffer
+		wg.Done()
+		io.Copy(&buf, reader)
+		out <- buf.String()
+	}()
+	wg.Wait()
+	f()
+	writer.Close()
+	return <-out
 }
