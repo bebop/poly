d1fcf3b3483606798d3ee0b6546c2f82b088c083
Timothy Stiles <tim@stiles.io>
2020-11-02 14:40:19 -0800

    added installation instructions to go docs.

---
+++ doc.go
@@ -0,0 +1,30 @@
+/*Package poly is a go package for engineering organisms.
+
+
+
+Poly can be used in two ways.
+
+	 1. As a Go library where you have finer control and can make magical things happen.
+	 2. As a command line utility where you can bash script your way to greatness and make DNA go brrrrrrrr.
+
+
+Installation
+
+
+These instructions assume that you already have a working go environment. If not see:
+	https://golang.org/doc/install
+
+
+Building Poly CLI and package from scratch:
+
+	git clone https://github.com/TimothyStiles/poly.git && cd poly && go build ./... && go install ./...
+
+
+
+Installing latest release of poly as a go package:
+	go get github.com/TimothyStiles/poly
+
+
+For CLI only instructions please checkout: https://pkg.go.dev/github.com/TimothyStiles/poly/poly
+*/
+package poly
---
+++ poly/doc.go
@@ -0,0 +1,15 @@
+/*
+
+Poly command line utility installation instructions:
+
+Mac OSX
+
+	brew install timothystiles/poly/poly
+
+Linux - deb/rpm
+	Download the .deb or .rpm from the releases page https://github.com/TimothyStiles/poly/releases and install with `dpkg -i` and `rpm -i` respectively
+
+Windows
+	Coming soon...
+*/
+package main
