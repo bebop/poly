2c3dec0f6a7d60e392c00ff166bac1a95a2cd9c2
Tim <TimothyStiles@users.noreply.github.com>
2020-11-12 09:40:56 -0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
     "Getting Started": ['installation'],
-    CLI: ['cli-converting', 'cli-hashing'],
-    Library: ['library-io', 'library-hashing'],
+    // CLI: ['cli-converting', 'cli-hashing'],
+    // Library: ['library-io', 'library-hashing'],
     // Contributing: ['doc'],
   },
 };
