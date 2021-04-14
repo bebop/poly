83ec572862dd6bdecd81ce672c81cd185847ab12
Keoni Gandall <koeng101@gmail.com>
2021-04-14 08:21:14 -0700

    Bot run again!

--- io_test.go
+++ io_test.go
@@ -419,7 +419,6 @@ func ExampleReadGbkMulti() {
 }
 
 func ExampleReadGbkFlat() {
-	//sequences := ReadGbkFlat("data/flatGbk_test.seq")
 	sequences := ReadGbkFlat("data/long_comment.seq")
 	var locus []string
 	for _, sequence := range sequences {
