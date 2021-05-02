e2757f542c0ed6a64ed402c729730fb1c0fa617d
Isaac Guerreiro <ig00114@saqueepague.com.br>
2021-05-02 19:04:23 -0300

    Corrections made based on the last review including comments, variable name changes and some links to wikipedia

--- sequence_test.go
+++ sequence_test.go
@@ -73,16 +73,16 @@ func TestLocationParser(t *testing.T) {
 }
 
 func ExampleRandomProteinSequence() {
-
+        // RandomProteinSequence builds a Protein Sequence by only passing through arguments a length and a seed that will be use to generate a randomly the sequence. The length needs to be greater than two because every sequence already have a start and stop codon. Seed makes this test deterministic.
 	randomProtein, _ := RandomProteinSequence(15, 2)
 	fmt.Println(randomProtein)
-	// Output: MGLLTAVNIFGNNP*
+	// Output: MHHPAFRMFNTMYG*
 }
 
 func TestRandomProteinSequence(t *testing.T) {
-        const n = 10
+        const length = 10
         const seed = 2
-        sequence, _ := RandomProteinSequence(n, seed)
+        sequence, _ := RandomProteinSequence(length, seed)
 
         if sequence[0] != 'M' {
             t.Errorf("Random sequence doesn't have the correct initial aminoacid in sequence 'RandomSequence(10, 2)'. Got this: \n%s instead of \n%s", string(sequence[0]), "M")
@@ -92,20 +92,20 @@ func TestRandomProteinSequence(t *testing.T) {
             t.Errorf("Random sequence doesn't have correct last aminoacid in sequence 'RandomSequence(10, 2)'. Got this: \n%s instead of \n%s", string(sequence[len(sequence)-1]), "*")
         }
 
-        if len(sequence) != n {
-            t.Errorf("Random sequence doesn't have the sequence size equal parameter passed through n 'RandomSequence(10, 2)'. Got this: \n%s instead of \n%s", strconv.Itoa(len(sequence)), strconv.Itoa(n))
+        if len(sequence) != length {
+            t.Errorf("Random sequence doesn't have the sequence size equal parameter passed through n 'RandomSequence(10, 2)'. Got this: \n%s instead of \n%s", strconv.Itoa(len(sequence)), strconv.Itoa(length))
         }
 }
 
 
 // Write a new case of test when you have a n inferior than 3
 func TestRandomProteinSequenceError (t *testing.T) {
-        const n = 2
+        const length = 2
         const seed = 4
-        sequence, _ := RandomProteinSequence(n, seed)
+        sequence, _ := RandomProteinSequence(length, seed)
 
         if len(sequence) != 0 {
-            t.Errorf("Random sequence must have sequence size equals 0 'RandomSequence(2, 4)'. Got this: \n%s instead of \n%s", strconv.Itoa(len(sequence)), strconv.Itoa(n))
+            t.Errorf("Random sequence must have sequence size equals 0 'RandomSequence(2, 4)'. Got this: \n%s instead of \n%s", strconv.Itoa(len(sequence)), strconv.Itoa(length))
         }
 }
 
