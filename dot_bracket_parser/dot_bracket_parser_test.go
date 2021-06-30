a4a8c0cf6e91e06222eb493d90a46a0ecb92cb62
Vivek Raja <vivek.r.raja@gmail.com>
2021-06-30 10:20:13 +0530

    initial parser code

---
+++ dot_bracket_parser/dot_bracket_parser_test.go
@@ -0,0 +1,43 @@
+package dot_bracket_parser
+
+import "fmt"
+
+func ExampleGetSecondaryStructure() {
+	dotBracket := "..((((...))))...((........)).."
+	annotatedStructure, secondaryStructure := SecondaryStructureFromDotBracket(dotBracket)
+	fmt.Println(annotatedStructure)
+	fmt.Println(DotBracket(secondaryStructure, 0) == dotBracket)
+	// Output:
+	// ee((((hhh))))eee((hhhhhhhh))ee
+	// true
+}
+
+func ExampleGetSecondaryStructure2() {
+	dotBracket := "(((((((((...((((((.........))))))........((((((.......))))))..)))))))))"
+	annotatedStructure, secondaryStructure := SecondaryStructureFromDotBracket(dotBracket)
+	fmt.Println(annotatedStructure)
+	fmt.Println(DotBracket(secondaryStructure, 0) == dotBracket)
+	// Output:
+	// (((((((((mmm((((((hhhhhhhhh))))))mmmmmmmm((((((hhhhhhh))))))mm)))))))))
+	// true
+}
+
+func ExampleGetSecondaryStructure3() {
+	dotBracket := "((((.((((......))))((((...))....)).))))"
+	annotatedStructure, secondaryStructure := SecondaryStructureFromDotBracket(dotBracket)
+	fmt.Println(annotatedStructure)
+	fmt.Println(DotBracket(secondaryStructure, 0) == dotBracket)
+	// Output:
+	// ((((m((((hhhhhh))))((((hhh))iiii))m))))
+	// true
+}
+
+func ExampleGetSecondaryStructure4() {
+	dotBracket := "......((((((.(((..(((((.(((....(((((......)))))..))).))))).)))....))))))..................."
+	annotatedStructure, secondaryStructure := SecondaryStructureFromDotBracket(dotBracket)
+	fmt.Println(annotatedStructure)
+	fmt.Println(DotBracket(secondaryStructure, 0) == dotBracket)
+	// Output:
+	// eeeeee((((((i(((ii(((((i(((iiii(((((hhhhhh)))))ii)))i)))))i)))iiii))))))eeeeeeeeeeeeeeeeeee
+	// true
+}
