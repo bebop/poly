a1e6980886940acd5e78758166631e133e522e65
Timothy Stiles <tim@stiles.io>
2021-06-30 18:31:36 -0700

    fixed grammatical error in JSON comments.

--- io/pson/pson.go
+++ io/pson/pson.go
@@ -13,7 +13,7 @@ JSON specific IO related things begin here.
 
 ******************************************************************************/
 
-// Parse parses an poly.Sequence JSON file and adds appropriate pointers to struct.
+// Parse parses a poly.Sequence JSON file and adds appropriate pointers to struct.
 func Parse(file []byte) poly.Sequence {
 	var sequence poly.Sequence
 	_ = json.Unmarshal([]byte(file), &sequence)
@@ -26,14 +26,14 @@ func Parse(file []byte) poly.Sequence {
 	return sequence
 }
 
-// Read reads an poly.Sequence JSON file.
+// Read reads a poly.Sequence JSON file.
 func Read(path string) poly.Sequence {
 	file, _ := ioutil.ReadFile(path)
 	sequence := Parse(file)
 	return sequence
 }
 
-// Write writes an poly.Sequence struct out to json.
+// Write writes a poly.Sequence struct out to json.
 func Write(sequence poly.Sequence, path string) {
 	file, _ := json.MarshalIndent(sequence, "", " ")
 	_ = ioutil.WriteFile(path, file, 0644)
