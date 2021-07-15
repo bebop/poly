c125feff573c2b8becc9e38d807398967ac5e6b4
Isaac Guerreiro <ig00114@saqueepague.com.br>
2021-07-15 17:57:26 -0300

    Adding GC content feature

--- checks/checks.go
+++ checks/checks.go
@@ -7,3 +7,22 @@ import "github.com/Open-Science-Global/poly/transform"
 func IsPalindromic(sequence string) bool {
 	return sequence == transform.ReverseComplement(sequence)
 }
+
+func gcContent(sequence string) float64 {
+
+	if len(sequence) < 1 {
+	  return 0.0
+	}
+  
+	const c = 67
+	const g = 71
+	count := 0.0
+  
+	for i, character := range(sequence) {
+	  if character == g || character == c {
+		count += 1.0 
+	  }
+	}
+  
+	return count / float64(len(sequence))
+  }
