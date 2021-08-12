1c9646c43e175d874f00f66fee8cad0bf0b3862b
Isaac Guerreiro <ig00114@saqueepague.com.br>
2021-08-12 19:34:27 -0300

    Create a feature to removes repetitions between cds and a external sequence as host genome or plasmid to avoid homology recombination

--- transform/transform_test.go
+++ transform/transform_test.go
@@ -1,6 +1,10 @@
 package transform
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
 
 func ExampleReverseComplement() {
 	sequence := "GATTACA"
@@ -24,4 +28,20 @@ func ExampleReverse() {
 	fmt.Println(reverse)
 
 	// Output: ACATTAG
+}
+
+func exampleKmerTable(t *testing.T) {
+	var kmers []string
+	kmerTable := GetKmerTable(2, "ATCG")
+
+	for k := range kmerTable {
+		kmers = append(kmers, k)
+	}
+
+	if len(kmers) == 3 && kmers[0] == "AT" && kmers[1] == "TC" && kmers[2] == "CG" {
+		t.Errorf("Expected kmers table to have length 3, got: %s", strconv.Itoa(len(kmers)))
+		t.Errorf("Expected kmers table to have first Element 1 AT, got: %s", kmers[0])
+		t.Errorf("Expected kmers table to have first Element 2 TC, got: %s", kmers[1])
+		t.Errorf("Expected kmers table to have first Element 3 CG, got: %s", kmers[2])
+	}
 }
