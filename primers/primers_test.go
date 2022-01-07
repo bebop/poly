e57a7506b45177366695f5361056e839e2f0dfc4
Tim <TimothyStiles@users.noreply.github.com>
2022-01-07 13:09:08 -0800

    Revert "Add GcContent function to barcodes (#214)"
    This reverts commit 5727c93e78735d114675acfab7e15b56814315c8.

--- primers/primers_test.go
+++ primers/primers_test.go
@@ -106,13 +106,6 @@ func ExampleCreateBarcodes() {
 	// Output: AAAATAAAGAAACAATTAAT
 }
 
-func ExampleCreateBarcodesGcRange() {
-	barcodes := CreateBarcodesGcRange(20, 4, .25, .75)
-
-	fmt.Println(barcodes[0])
-	// Output: GAAACAATTAATGAATCAAG
-}
-
 func TestCreateBarcode(t *testing.T) {
 	testFunc := func(s string) bool {
 		return !strings.Contains(s, "GGCCGCGCCCC")
