0812176a9c1ee7ed977fc09f76df4997f92ff5e5
Fergal <99358142+fergloragain@users.noreply.github.com>
2022-03-23 22:04:43 +0000

    Increase coverage for the variants package
    * Fix a typo for iteratitive -> iterative
    
    * Rename the test package from variants_test to variants to permit testing of private function
    
    * Increase test coverage to 100%
    
    * Use the assert package to fail tests, as this is more succinct than if statements

--- transform/variants/variants.go
+++ transform/variants/variants.go
@@ -53,24 +53,25 @@ func AllVariantsIUPAC(seq string) ([]string, error) {
 	}
 	return seqVariants, nil
 }
-func cartRune(inList ...[]rune) [][]rune {
-	// An iteratitive approach to calculate Cartesian product of two or more lists
+
+func cartRune(inLists ...[]rune) [][]rune {
+	// An iterative approach to calculate Cartesian product of two or more lists
 	// Adapted from https://rosettacode.org/wiki/Cartesian_product_of_two_or_more_lists
 	// supposedly "minimizes allocations and computes and fills the result sequentially"
 
 	var possibleVariants int = 1 // a counter used to determine the possible number of variants
-	for _, inList := range inList {
+	for _, inList := range inLists {
 		possibleVariants *= len(inList)
 	}
 	if possibleVariants == 0 {
 		return nil // in the future this could be part of an error return?
 	}
-	allVariants := make([][]rune, possibleVariants)              // this is the 2D slice where all variants will be stored
-	variantHolders := make([]rune, possibleVariants*len(inList)) // this is an empty slice with a length totaling the size of all input characters
-	variantChoices := make([]int, len(inList))                   // these will be all the possible variants
+	allVariants := make([][]rune, possibleVariants)               // this is the 2D slice where all variants will be stored
+	variantHolders := make([]rune, possibleVariants*len(inLists)) // this is an empty slice with a length totaling the size of all input characters
+	variantChoices := make([]int, len(inLists))                   // these will be all the possible variants
 	start := 0
 	for variant := range allVariants {
-		end := start + len(inList) // define end point
+		end := start + len(inLists) // define end point
 		variantHolder := variantHolders[start:end]
 
 		allVariants[variant] = variantHolder
@@ -78,11 +79,11 @@ func cartRune(inList ...[]rune) [][]rune {
 		start = end // start at end point
 
 		for variantChoicesIndex, variantChoice := range variantChoices {
-			variantHolder[variantChoicesIndex] = inList[variantChoicesIndex][variantChoice]
+			variantHolder[variantChoicesIndex] = inLists[variantChoicesIndex][variantChoice]
 		}
 		for variantChoicesIndex := len(variantChoices) - 1; variantChoicesIndex >= 0; variantChoicesIndex-- {
 			variantChoices[variantChoicesIndex]++
-			if variantChoices[variantChoicesIndex] < len(inList[variantChoicesIndex]) {
+			if variantChoices[variantChoicesIndex] < len(inLists[variantChoicesIndex]) {
 				break
 			}
 			variantChoices[variantChoicesIndex] = 0
