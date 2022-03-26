157474911ae199a505569f539626f3ef4a7a1698
fergloragain <99358142+fergloragain@users.noreply.github.com>
2022-03-26 16:52:25 +0000

    Increase coverage for the variants package (#245)
    * Fix a typo for iteratitive -> iterative
    
    * Rename the test package from variants_test to variants to permit testing of private function
    
    * Increase test coverage to 100%
    
    * Use the assert package to fail tests, as this is more succinct than if statements

--- transform/variants/variants_test.go
+++ transform/variants/variants_test.go
@@ -1,30 +1,32 @@
-package variants_test
+package variants
 
 import (
 	"fmt"
-	"log"
 	"sort"
 	"testing"
 
-	"github.com/TimothyStiles/poly/transform/variants"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIUPAC(t *testing.T) {
 	testSeq := "ATN"
 	testVariants := []string{"ATG", "ATA", "ATT", "ATC"}
-	testVariantsIUPAC, err := variants.AllVariantsIUPAC(testSeq)
-	if err != nil {
-		log.Fatal(err)
-	}
+	testVariantsIUPAC, err := AllVariantsIUPAC(testSeq)
+	assert.Nil(t, err)
 
 	sort.Strings(testVariants)
 	sort.Strings(testVariantsIUPAC)
 
 	for index := range testVariants {
-		if testVariants[index] != testVariantsIUPAC[index] {
-			t.Errorf("IUPAC variant has changed on test 'allIUPAC('ATN')'. Got this:\n%s instead of \n%s", testVariantsIUPAC, testVariants)
-		}
+		assert.Equal(t, testVariants[index], testVariantsIUPAC[index], "IUPAC variant has changed")
 	}
+}
+
+func TestIUPAC_errors(t *testing.T) {
+	testSeq := "ATX"
+	seqVariants, err := AllVariantsIUPAC(testSeq)
+	assert.NotNil(t, err, "expected error for unsupported IUPAC character, got nil")
+	assert.Equal(t, seqVariants, []string{})
 }
 
 func ExampleAllVariantsIUPAC() {
@@ -32,8 +34,62 @@ func ExampleAllVariantsIUPAC() {
 	// and returns all iupac variants as output
 	mendelIUPAC := "ATGGARAAYGAYGARCTN"
 	// ambiguous IUPAC codes for most of the sequences that code for the protein MENDEL
-	mendelIUPACvariants, _ := variants.AllVariantsIUPAC(mendelIUPAC)
+	mendelIUPACvariants, _ := AllVariantsIUPAC(mendelIUPAC)
 	fmt.Println(mendelIUPACvariants)
 	// Output: [ATGGAGAATGATGAGCTG ATGGAGAATGATGAGCTA ATGGAGAATGATGAGCTT ATGGAGAATGATGAGCTC ATGGAGAATGATGAACTG ATGGAGAATGATGAACTA ATGGAGAATGATGAACTT ATGGAGAATGATGAACTC ATGGAGAATGACGAGCTG ATGGAGAATGACGAGCTA ATGGAGAATGACGAGCTT ATGGAGAATGACGAGCTC ATGGAGAATGACGAACTG ATGGAGAATGACGAACTA ATGGAGAATGACGAACTT ATGGAGAATGACGAACTC ATGGAGAACGATGAGCTG ATGGAGAACGATGAGCTA ATGGAGAACGATGAGCTT ATGGAGAACGATGAGCTC ATGGAGAACGATGAACTG ATGGAGAACGATGAACTA ATGGAGAACGATGAACTT ATGGAGAACGATGAACTC ATGGAGAACGACGAGCTG ATGGAGAACGACGAGCTA ATGGAGAACGACGAGCTT ATGGAGAACGACGAGCTC ATGGAGAACGACGAACTG ATGGAGAACGACGAACTA ATGGAGAACGACGAACTT ATGGAGAACGACGAACTC ATGGAAAATGATGAGCTG ATGGAAAATGATGAGCTA ATGGAAAATGATGAGCTT ATGGAAAATGATGAGCTC ATGGAAAATGATGAACTG ATGGAAAATGATGAACTA ATGGAAAATGATGAACTT ATGGAAAATGATGAACTC ATGGAAAATGACGAGCTG ATGGAAAATGACGAGCTA ATGGAAAATGACGAGCTT ATGGAAAATGACGAGCTC ATGGAAAATGACGAACTG ATGGAAAATGACGAACTA ATGGAAAATGACGAACTT ATGGAAAATGACGAACTC ATGGAAAACGATGAGCTG ATGGAAAACGATGAGCTA ATGGAAAACGATGAGCTT ATGGAAAACGATGAGCTC ATGGAAAACGATGAACTG ATGGAAAACGATGAACTA ATGGAAAACGATGAACTT ATGGAAAACGATGAACTC ATGGAAAACGACGAGCTG ATGGAAAACGACGAGCTA ATGGAAAACGACGAGCTT ATGGAAAACGACGAGCTC ATGGAAAACGACGAACTG ATGGAAAACGACGAACTA ATGGAAAACGACGAACTT ATGGAAAACGACGAACTC]
+}
 
+func ExampleAllVariantsIUPAC_error() {
+	// AllVariantsIUPAC takes a string as input
+	// and returns all iupac variants as output
+	mendelIUPAC := "ATGGARAAYGAYGARXYZ"
+	// ambiguous IUPAC codes for most of the sequences that code for the protein MENDEL
+	mendelIUPACvariants, _ := AllVariantsIUPAC(mendelIUPAC)
+	fmt.Println(mendelIUPACvariants)
+	// Output: []
+}
+
+func Test_cartRunes(t *testing.T) {
+	for _, tc := range []struct {
+		in  [][]rune
+		out [][]rune
+	}{
+		{
+			in: [][]rune{
+				{1, 2, 3},
+			},
+			out: [][]rune{
+				{1},
+				{2},
+				{3},
+			},
+		},
+		{
+			in:  [][]rune{nil},
+			out: nil,
+		},
+		{
+			in: [][]rune{
+				{'A', 'T', 'G'},
+				{'A', 'T', 'A', 'G'},
+			},
+			out: [][]rune{
+				{'A', 'A'},
+				{'A', 'T'},
+				{'A', 'A'},
+				{'A', 'G'},
+				{'T', 'A'},
+				{'T', 'T'},
+				{'T', 'A'},
+				{'T', 'G'},
+				{'G', 'A'},
+				{'G', 'T'},
+				{'G', 'A'},
+				{'G', 'G'},
+			},
+		},
+	} {
+		runes := cartRune(tc.in...)
+		assert.Equal(t, runes, tc.out)
+	}
 }
