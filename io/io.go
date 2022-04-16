79a26f52d7682559878239a12a66b7796a0d6a3f
Tim <TimothyStiles@users.noreply.github.com>
2022-04-16 12:24:23 -0700

    fill in missing package strings. (#250)
    * Create io.go
    
    * added docstrings.

---
+++ io/io.go
@@ -0,0 +1,9 @@
+/*
+Package io provides utilities for reading and writing sequence data.
+*/
+package io
+
+/*
+This package is supposed to be empty and only exists to provide a doc string.
+Otherwise its namespace would collide with Go's native IO package.
+*/
---
+++ synthesis/synthesis.go
@@ -0,0 +1,9 @@
+/*
+Package synthesis provides utilities for preparing DNA to be synthesized.
+*/
+package synthesis
+
+/*
+This package is supposed to be empty and only exists to provide a doc string.
+Otherwise its namespace would collide with Go's native IO package.
+*/
