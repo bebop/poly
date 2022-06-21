93df79ffc0db2650fad651895ee6df5713f932e1
Timothy Stiles <tim@stiles.io>
2022-06-21 07:06:36 -0700

    fixed typo in doc.go

--- doc.go
+++ doc.go
@@ -10,7 +10,7 @@ suite of common functions for engineering organisms
 through a simple interface in Go.
 
 The reason why Go was choosen is that it's a highly popular
-language with a simple sytax and a large ecosystem of packages
+language with a simple syntax and a large ecosystem of packages
 that makes it *extremely* easy to learn, use, and deploy.
 It's also on average 25X faster and more energy efficient that
 Python.
