a0faa66eebb9a648b787070ca48994aecb95444d
Timothy Stiles <tim@stiles.io>
2023-01-04 11:58:23 -0800

    initial commit for dna_parts_test.go

--- tutorials/002_dna_parts_test.go
+++ tutorials/002_dna_parts_test.go
@@ -1,3 +1,82 @@
 package tutorials_test
 
 // TODO: Write a tutorial on DNA parts
+
+/******************************************************************************
+Jan, 4, 2023
+
+
+== We are tiny machines ==
+
+"Holy f**
+Tiny machines. We are tiny machines"
+
+- OnlyLaTeX#3347 on Poly's Discord
+https://discord.com/channels/723589432627953805/746120460978356379/1046981178105790554
+
+Almost every public speaking event I do I end up explaining how we are in fact
+the gray goo. Most of your body's dry weight is composed of nanobots that
+biologists for some reason refer to as proteins despite nanobots being the
+perfect description for them.
+
+There are motor proteins that spin and look almost exactly like tiny electric
+motors. There are enzymes that are basically tiny chemical reactors. Heck there
+are even walker proteins that literally walk around and do stuff.
+
+We are made of tiny machines and the instruction sets for these tiny machines.
+
+In general DNA stores the instruction sets for these tiny machines which is what
+we're working with in this tutorial. RNA is used to transport those instructions
+to a class of nanobots called ribosomes which then use those instructions to
+create, you guessed it, more nanobots. This process is known as the,
+"Central Dogma of Biology". Check out this sweet animation to get a better
+idea of what's going on.
+
+https://www.youtube.com/watch?v=9kOGOY7vthk
+
+
+It turns out that DNA is a really good way to store information and it doesn't
+just store information about how to make proteins. Non-protein producing sequences
+can regulate gene expression, act as binding sites for other proteins, and
+much more.
+
+Here's a short list of the kinds of non-coding elements found in DNA :
+
+Promoters
+https://en.wikipedia.org/wiki/Promoter_(genetics)
+
+Enhancers
+https://en.wikipedia.org/wiki/Enhancer_(genetics)
+
+Silencers
+https://en.wikipedia.org/wiki/Silencer_(genetics)
+
+Insulators
+https://en.wikipedia.org/wiki/Insulator_(genetics)
+
+Primer Binding Sites
+https://en.wikipedia.org/wiki/Primer_(molecular_biology)
+
+Ribosome Binding Sites
+https://en.wikipedia.org/wiki/Ribosome-binding_site
+
+Restriction Enzyme Binding Sites
+https://en.wikipedia.org/wiki/Restriction_enzyme
+
+With the exception of restriction enzyme binding sites these are all
+features that you'll often find within the annotated features
+of a genome. Often restriction enzyme binding sites are not within
+the feature set itself but instead are detectec via regex in most
+popular DNA editing tools.
+
+TTFN,
+Tim
+
+P.S.
+
+Synthetic biologists sometimes refer to these DNA sequences as "DNA parts" and
+the organisms they work with as "chassis". These terms aren't commonly used
+outside the field and I won't be using them in this tutorial but I wanted to
+mention them here so you know what they mean if you see them in the wild.
+
+******************************************************************************/
