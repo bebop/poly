9f2ba60ee10472e6fbe6df1f8fae2416fa1b8090
Timothy Stiles <tim@stiles.io>
2023-02-24 12:53:57 -0800

    added Smith-Waterman example test.

--- align/example_test.go
+++ align/example_test.go
@@ -12,4 +12,15 @@ func ExampleNeedlemanWunsch() {
 	scoring := align.NewScoring()
 	score, alignA, alignB := align.NeedlemanWunsch(a, b, scoring)
 	fmt.Printf("score: %d, A: %s, B: %s", score, alignA, alignB)
+
+	// Output: score: 0, A: G-ATTACA, B: GCA-TGCU
+}
+
+func ExampleSmithWaterman() {
+	a := "GATTACA"
+	b := "GCATGCU"
+	scoring := align.NewScoring()
+	score, alignA, alignB := align.SmithWaterman(a, b, scoring)
+	fmt.Printf("score: %d, A: %s, B: %s", score, alignA, alignB)
+	// Output: score: 2, A: AT, B: AT
 }
