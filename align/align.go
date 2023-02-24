ecacf849870619230ce9af67193e61692e30ed6f
Timothy Stiles <tim@stiles.io>
2023-02-24 12:47:34 -0800

    added docstring for alignment package.

--- align/align.go
+++ align/align.go
@@ -1,3 +1,62 @@
+/*
+Package align is a package for aligning (comparing) DNA, RNA, and protein sequences.
+
+Biology is fickle and full of quirks that make it hard to do even the most basic of tasks
+which we would normally take for granted when working with other kinds of data.
+
+Comparing two biogical sequences to see if they're roughly equivalent is one of those tasks.
+
+Essentially two almost identical sequences with almost identical functionality can contain
+small insertions or deletions that shift the entire string such that a meaningful comparison via
+hamming distance or levenshtein distance becomes impossible.
+
+For example:
+
+Timothy Stiles
+||||||| ||||||
+Timothy Stiles
+
+is an easy match with hamming or levenshtein distance.
+
+However, say we introduce a new character to the beginning of the sequence.
+
+Timothy Stiles
+xxxxxxxxxxxxxxxx
+A Timothy Stiles
+
+Now our edit distance via levenshtein is maximized at 16 and we wouldn't
+be able to tell that semantically these strings are almost identical.
+
+This frame shifting seen above is incredibly common within biological sequences and alignment
+algorithms are designed in part to deal with these shifts so that when we compare two sequences
+like the two below we can get a more useful edit distance.
+
+GAAAAAAT
+GAA----T
+
+As of writing this package includes the two most basic algorithms for alignment,
+Needleman-Wunsch and Smith-Waterman.
+
+Both are "dynamic programming algorithms" which is a fancy 1980's term for they use
+matrices. If you're familiar with kernel operations, linear filters, or whatever term
+ML researchers are using nowadays for, "slide a window over a matrix and determine that
+entry's values using its neighbor's values", then this should be pretty easy to grock.
+
+If not these algorithms essentially compare every character in one sequence with another
+sequence and create an edit distance along with human readable string to show gaps like the
+previous example.
+
+I'm not really an expert on alignment so if you want to learn more about this class of algorithms
+wikipedia has a decent overview.
+
+https://en.wikipedia.org/wiki/Sequence_alignment
+
+Even if I may not know the answer to your alignment questions please ask and I'll do my best
+to help!
+
+TTFN,
+Tim
+*/
 package align
 
 // Scoring is a struct that holds the scoring matrix for match, mismatch, and gap penalties.
