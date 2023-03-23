2721a4aedd17665212a1dd5cc2627938cec9f374
Timothy Stiles <tim@stiles.io>
2023-03-23 15:58:01 -0700

    renamed variables and added docstring.

--- align/matrix/matrix.go
+++ align/matrix/matrix.go
@@ -1,3 +1,7 @@
+/*
+Package matrix provides a struct for substitution matrices and a struct for scoring matrices.
+*/
+
 package matrix
 
 import (
@@ -6,27 +10,30 @@ import (
 	"github.com/TimothyStiles/poly/alphabet"
 )
 
+// SubstitutionMatrix is a struct that holds a substitution matrix and the two alphabets that the matrix is defined over.
 type SubstitutionMatrix struct {
-	alpha1 *alphabet.Alphabet
-	alpha2 *alphabet.Alphabet
-	scores [][]int
+	FirstAlphabet  *alphabet.Alphabet
+	SecondAlphabet *alphabet.Alphabet
+	scores         [][]int
 }
 
-func NewSubstitutionMatrix(alpha1, alpha2 *alphabet.Alphabet, scores [][]int) (*SubstitutionMatrix, error) {
-	if len(alpha1.Symbols()) != len(scores) || len(alpha2.Symbols()) != len(scores[0]) {
+// NewSubstitutionMatrix creates a new substitution matrix from two alphabets and a 2D array of scores.
+func NewSubstitutionMatrix(firstAlphabet, secondAlphabet *alphabet.Alphabet, scores [][]int) (*SubstitutionMatrix, error) {
+	if len(firstAlphabet.Symbols()) != len(scores) || len(secondAlphabet.Symbols()) != len(scores[0]) {
 		return nil, fmt.Errorf("invalid dimensions of substitution matrix")
 	}
-	return &SubstitutionMatrix{alpha1, alpha2, scores}, nil
+	return &SubstitutionMatrix{firstAlphabet, secondAlphabet, scores}, nil
 }
 
-func (s *SubstitutionMatrix) Score(a, b string) (int, error) {
-	i, err := s.alpha1.Encode(a)
+// Score returns the score of two symbols in the substitution matrix.
+func (matrix *SubstitutionMatrix) Score(a, b string) (int, error) {
+	firstSymbolIndex, err := matrix.FirstAlphabet.Encode(a)
 	if err != nil {
 		return 0, err
 	}
-	j, err := s.alpha2.Encode(b)
+	secondSymbolIndex, err := matrix.SecondAlphabet.Encode(b)
 	if err != nil {
 		return 0, err
 	}
-	return s.scores[i][j], nil
+	return matrix.scores[firstSymbolIndex][secondSymbolIndex], nil
 }
