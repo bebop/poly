36f255cba7c2277dcfb7946038a6d9769263eb06
Maximilien Rothier Bautzer <m.rothierbautzer@gmail.com>
2023-11-08 17:31:02 +0000

    UpdateWeights test

--- synthesis/codon/codon_test.go
+++ synthesis/codon/codon_test.go
@@ -412,3 +412,78 @@ func TestNewAminoAcidChooser(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdateWeights(t *testing.T) {
+	var (
+		mockError = errors.New("new chooser rigged to fail")
+	)
+
+	tests := []struct {
+		name string
+
+		aminoAcids []AminoAcid
+
+		chooserFn func(choices ...weightedRand.Choice) (*weightedRand.Chooser, error)
+
+		wantErr error
+	}{
+		{
+			name: "ok",
+
+			aminoAcids: []AminoAcid{
+				{
+					Letter: "R",
+					Codons: []Codon{
+						{
+							Triplet: "CGU",
+							Weight:  1,
+						},
+					},
+				},
+			},
+
+			chooserFn: weightedRand.NewChooser,
+
+			wantErr: nil,
+		},
+		{
+			name: "chooser fn constructor error",
+
+			aminoAcids: []AminoAcid{
+				{
+					Letter: "R",
+					Codons: []Codon{
+						{
+							Triplet: "CGU",
+							Weight:  1,
+						},
+					},
+				},
+			},
+
+			chooserFn: func(choices ...weightedRand.Choice) (*weightedRand.Chooser, error) {
+				return nil, mockError
+			},
+
+			wantErr: mockError,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// replace chooser fn with test one
+			newChooserFn = tt.chooserFn
+
+			defer func() {
+				newChooserFn = weightedRand.NewChooser
+			}()
+
+			optimizationTable := NewTranslationTable(11)
+
+			err := optimizationTable.UpdateWeights(tt.aminoAcids)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("got %v, want %v", err, tt.wantErr)
+			}
+		})
+	}
+}
