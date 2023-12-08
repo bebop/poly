ff0fec93aa6ff92fea6aaf314d127f0424ecca64
Trenton <trenflem@gmail.com>
2023-12-08 00:43:06 -0500

    wavelet tree doc and address todos

--- bwt/wavelet.go
+++ bwt/wavelet.go
@@ -1,18 +1,21 @@
 package bwt
 
 import (
+	"fmt"
 	"math"
 
 	"golang.org/x/exp/slices"
 )
 
+// waveletTree datastructure that allows us to
+// conduct RSA queries on strings.
 type waveletTree struct {
 	root  *node
 	alpha []charInfo
 }
 
-// TODO: figure out empty nodes case
-// TODO: figure out out of bounds case
+// Access will return the ith character of the original
+// string used to build the waveletTree
 func (wt waveletTree) Access(i int) byte {
 	curr := wt.root
 	for !curr.isLeaf() {
@@ -27,8 +30,8 @@ func (wt waveletTree) Access(i int) byte {
 	return curr.char
 }
 
-// TODO: deal with bad lookup char
-// TODO: deal with somehow bad path
+// Rank allows us to get the rank of a specified character in
+// the original string
 func (wt waveletTree) Rank(char byte, i int) int {
 	curr := wt.root
 	ci := wt.lookupCharInfo(char)
@@ -48,6 +51,8 @@ func (wt waveletTree) Rank(char byte, i int) int {
 	return rank
 }
 
+// Select allows us to get the corresponding posisiton of a character
+// in the original string given its rank.
 func (wt waveletTree) Select(char byte, rank int) int {
 	curr := wt.root
 	ci := wt.lookupCharInfo(char)
@@ -67,8 +72,11 @@ func (wt waveletTree) Select(char byte, rank int) int {
 		curr = curr.parent
 		level--
 		pathBit := ci.path.getBit(ci.path.len() - 1 - level)
-		// TODO: do we really need the ok on the select?
-		rank, _ = curr.data.Select(pathBit, rank)
+		rank, ok := curr.data.Select(pathBit, rank)
+		if !ok {
+			msg := fmt.Sprintf("could not find a correspodning bit for node.Select(%t, %d) for characterInfo %+v", pathBit, rank, ci)
+			panic(msg)
+		}
 	}
 
 	return rank
@@ -80,11 +88,10 @@ func (wt waveletTree) lookupCharInfo(char byte) charInfo {
 			return wt.alpha[i]
 		}
 	}
-	panic("better messaging or handling")
+	msg := fmt.Sprintf("could not find character %s in alphabet %+v. this should not be possible and indicates that the WaveletTree is malformed", string(char), wt.alpha)
+	panic(msg)
 }
 
-// TODO: talk about how we could probably greaty improve performance with one big bit vector that
-// represents the whole tree by concatenation the level order traversal of each node's bits
 type node struct {
 	data   rsaBitVector
 	char   byte
@@ -168,7 +175,10 @@ func isInAlpha(alpha []charInfo, b byte) bool {
 	return false
 }
 
-// TODO: talk about arranging OG alpha such that we minimize memory
+// partitionAlpha partitions the alaphabet in half based on whether its corresponding path bit
+// is a 0 or 1. 0 with comprise the left tree while 1 will comprise the right. The alphabet
+// should be sorted in such a way that we remove the most amount of characters nearest to the
+// root of the tree to reduce the memory footprint as much as possible.
 func partitionAlpha(currentLevel int, alpha []charInfo) (left []charInfo, right []charInfo) {
 	for _, a := range alpha {
 		if a.path.getBit(a.path.len() - 1 - currentLevel) {
@@ -181,19 +191,14 @@ func partitionAlpha(currentLevel int, alpha []charInfo) (left []charInfo, right 
 	return left, right
 }
 
-func getLeft(nodePos int) int {
-	return nodePos*2 + 1
-}
-
-func getRight(nodePos int) int {
-	return nodePos*2 + 2
-}
-
-func getParent(nodePos int) int {
-	return (nodePos + 1) / 2
-}
-
-// alphabets are expected to be small for real usecases
+// getCharInfoDescByRank takes in the bytes of the original
+// string and return a sorted list of character metadata descending
+// by rank. The character metadata is important for building the rest
+// of the tree along with quering it later on. The sorting is important
+// because this allows us to build the tree in the most memory efficient
+// way since the characters with the greatest counts will be removed first
+// before build the subsequent nodes in the lower levels.
+// NOTE: alphabets are expected to be small for real usecases
 func getCharInfoDescByRank(b []byte) []charInfo {
 	ranks := make(map[byte]int)
 	for i := 0; i < len(b); i++ {
@@ -226,6 +231,28 @@ func getCharInfoDescByRank(b []byte) []charInfo {
 	return sortedInfo
 }
 
+// encodeCharPathIntoBitVector important metadata to understand
+// which character we are woring with in a given path in the tree.
+// For example, given the alphabet A B C D E F G, a possible encoding is:
+// A: 000
+// B: 001
+// C: 010
+// D: 011
+// E: 100
+// F: 101
+// G: 110
+// H: 111
+//
+// If we wanted to get to the leaf that represent the character D, we'd
+// take the path:
+//
+//	   root
+//	  /
+//	left
+//	  \
+//	 right
+//	    \
+//	   right
 func encodeCharPathIntoBitVector(bv bitvector, n uint64) {
 	shift := 0
 	for n>>shift > 0 {
